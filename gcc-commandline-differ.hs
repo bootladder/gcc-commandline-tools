6cce142220465cd89fb6c6230f11578ac886380c
first last <yithkai@gmail.com>
2018-12-27 19:51:37 -0800

    works pretty good

--- gcc-commandline-differ.hs
+++ gcc-commandline-differ.hs
@@ -1,14 +1,18 @@
+import Data.List
 main = do
-  file <- readFile "input.txt"
-  let gcclines = lines file
-  mapM printTokensOfLine gcclines
+  file1 <- readFile "input1.txt"
+  file2 <- readFile "input2.txt"
+  let
+      filterTokens = filter (not . isPrefixOf "-I")
+      diffTokens l1 l2 = filter (\x -> not $ elem x l2) l1
 
-printTokensOfLine = printTokens . words
+      filteredTokens1 = filterTokens $ words file1
+      filteredTokens2 = filterTokens $ words file2
+      t1 = diffTokens filteredTokens1 filteredTokens2
+      t2 = diffTokens filteredTokens2 filteredTokens1
 
-printTokens [] = return ()
-printTokens (x:xs) =
-  do
-    putStrLn x
-    printTokens xs
-
-
+  putStrLn "Tokens in 1, not in 2"
+  mapM putStrLn t1
+  putStrLn "\n\nTokens in 2, not in 1"
+  mapM putStrLn t2
+  return ()
